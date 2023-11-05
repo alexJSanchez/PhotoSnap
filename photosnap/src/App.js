fd0fd276d23265b110747c2a48d93ac0579be875
alex sanchez <alexjsanchez1020@gmail.com>
2023-11-05 16:27:39 -0500

    trouble adding the correct font

--- photosnap/src/App.js
+++ photosnap/src/App.js
@@ -21,7 +21,7 @@ function App() {
 						<img className="create-and-share" src={create_and_share} />
 					</div>
 					<div className="main-card-one-bottom">
-						<h2>Create and share your photo Stories </h2>
+						<h2>Create and share your photo Stories. </h2>
 						<p>
 							Photosnap is a platform for photographers and visual storytellers.
 							We make it easy to share photos, tell stories and connect with
