3a7fa15e42b36533940f82423ec1999f30989bfa
alex sanchez <alexjsanchez1020@gmail.com>
2023-11-09 15:01:51 -0500

    main page stable

--- photosnap/src/App.js
+++ photosnap/src/App.js
@@ -7,6 +7,11 @@ import designed_for_everyone from "./assets/home/mobile/designed-for-everyone.jp
 import responsive from "./assets/features/desktop/responsive.svg";
 import infinite from "./assets/features/desktop/no-limit.svg";
 import bullhorn from "./assets/features/desktop/embed.svg";
+import facebook from "./assets/shared/desktop/facebook.svg";
+import youtube from "./assets/shared/desktop/youtube.svg";
+import twitter from "./assets/shared/desktop/twitter.svg";
+import pinterest from "./assets/shared/desktop/pinterest.svg";
+import instagram from "./assets/shared/desktop/instagram.svg";
 import "./App.css";
 
 function App() {
@@ -159,6 +164,29 @@ function App() {
 					</div>
 				</div>
 			</main>
+			<footer>
+				<img className="logo-light" src={logo} />
+				<div>
+					<img src={facebook} />
+					<img src={youtube} />
+					<img src={twitter} />
+					<img src={pinterest} />
+					<img src={instagram} />
+				</div>
+				<ol>
+					<li>home</li>
+					<li>stories</li>
+					<li>features</li>
+					<li>pricing</li>
+				</ol>
+				<div className="btn-four-box">
+					<button className="btn-four">
+						Get An Invite
+						<img className="arrow-img-light" src={arrow} />
+					</button>
+				</div>
+				<p>Copyright 2023. All Rights Reserved</p>
+			</footer>
 		</div>
 	);
 }
