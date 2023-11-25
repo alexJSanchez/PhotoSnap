eb59777ffd4cceb3380219b99879c03efdb84b7e
alexjsanchez1020 <alexjsanchez1020@gmail.com>
2023-11-25 03:23:04 -0500

    drop down adjustment

--- photosnap/src/pages/Navigation.js
+++ photosnap/src/pages/Navigation.js
@@ -1,33 +1,38 @@
-import React from "react";
+import { React, useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/shared/desktop/logo.svg";
 import mobile_menu from "../assets/shared/mobile/menu.svg";
 
 export default function Navigation() {
-	return (
-		<nav className="dropdown">
-			<div className="nav">
-				<div className="nav-left">
-					<img src={logo} />
-				</div>
-				<div className="nav-right">
-					<img className="link" src={mobile_menu} />
-				</div>
-			</div>
-			<div className="dropdown-menu flex flex-col items-center">
-				<Link to="/">
-					<h2>Home</h2>
-				</Link>
-				<Link to="stories">
-					<h2>Stories</h2>
-				</Link>
-				<Link to="features">
-					<h2>Features</h2>
-				</Link>
-				<Link to="pricing">
-					<h2>Pricing</h2>
-				</Link>
-			</div>
-		</nav>
-	);
+  const [dropdown, SetDropdown] = useState(true);
+  return (
+    <nav className="dropdown">
+      <div className="nav">
+        <div className="nav-left">
+          <img src={logo} />
+        </div>
+        <div className="nav-right">
+          <img className="link" src={mobile_menu} />
+        </div>
+      </div>
+      {dropdown ? (
+        <div className="dropdown-menu flex flex-col items-center">
+          <Link to="/">
+            <h2>Home</h2>
+          </Link>
+          <Link to="stories">
+            <h2>Stories</h2>
+          </Link>
+          <Link to="features">
+            <h2>Features</h2>
+          </Link>
+          <Link to="pricing">
+            <h2>Pricing</h2>
+          </Link>
+        </div>
+      ) : (
+        <></>
+      )}
+    </nav>
+  );
 }
