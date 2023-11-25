5d2b6e6b76b8af989bf5821d9b0f2ab1acad973a
alexjsanchez1020 <alexjsanchez1020@gmail.com>
2023-11-25 15:36:37 -0500

    add moon image

--- photosnap/src/pages/Stories.js
+++ photosnap/src/pages/Stories.js
@@ -1,5 +1,30 @@
 import React from "react";
-
+import moon from "../assets/stories/mobile/moon-of-appalacia.jpg";
+import arrow from "../assets/shared/desktop/arrow.svg";
 export default function Stories() {
-	return <div>Stories</div>;
+  return (
+    <div>
+      <div className="main-card">
+        <div className="main-card-top">
+          <img className="w100" src={moon} />
+        </div>
+        <div className="main-card-bottom">
+          <h4>LAST MONTH’S FEATURED STORY</h4>
+          <h2>HAZY FULL MOON OF APPALACHIA </h2>
+
+          <p>
+            Photosnap is a platform for photographers and visual storytellers.
+            We make it easy to share photos, tell stories and connect with
+            others.
+          </p>
+          <div className="btn-four-box">
+            <button className="btn-four">
+              Get An Invite
+              <img className="arrow-img-light" src={arrow} />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
