79488bee381ed2b7d75c021d8b5c4b43c77a7472
alex sanchez <alexjsanchez1020@gmail.com>
2023-11-26 17:25:14 -0500

    stories css

--- photosnap/src/pages/Stories.js
+++ photosnap/src/pages/Stories.js
@@ -4,7 +4,33 @@ import arrow from "../assets/shared/desktop/arrow.svg";
 export default function Stories() {
 	return (
 		<div>
-			<img src={moon} />
+			<div className="text-white flex justify-center flex-col">
+				<img src={moon} />
+				<div className="bg-black px-[29px]">
+					<h4 className="text-[12px] font-[700] tracking-[2px]">
+						LAST MONTH’S FEATURED STORY
+					</h4>
+					<h2 className="text-[32px] font-[700] leading-[40px] tracking-[3.33px]">
+						HAZY FULL MOON OF APPALACHIA
+					</h2>
+					<div className="flex text-[13px] opacity-[.75] gap-2">
+						<p>March 2nd 2020</p>
+						<p>by John Appleseed</p>
+					</div>
+					<p className="text-[15px] opacity-[.60] leading-[25px]">
+						The dissected plateau area, while not actually made up of geological
+						mountains, is popularly called "mountains," especially in eastern
+						Kentucky and West Virginia, and while the ridges are not high, the
+						terrain is extremely rugged.
+					</p>
+					<div className="btn-two-box">
+						<button className="btn-two-light">
+							READ STORY
+							<img className="arrow-img-light" src={arrow} />
+						</button>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
