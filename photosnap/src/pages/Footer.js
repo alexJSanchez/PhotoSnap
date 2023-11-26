ec21386254bb082f027978c409a4b9befce38f38
alex sanchez <alexjsanchez1020@gmail.com>
2023-11-26 15:24:26 -0500

    adjust css

--- photosnap/src/pages/Footer.js
+++ photosnap/src/pages/Footer.js
@@ -8,28 +8,30 @@ import instagram from "../assets/shared/desktop/instagram.svg";
 import logo from "../assets/shared/desktop/logo.svg";
 export default function Footer() {
 	return (
-		<footer>
+		<footer className="flex flex-col py-[3.5rem] bg-black">
 			<img className="logo-light" src={logo} />
-			<div>
+			<div className="mt-[2rem] mb-[3rem] gap-[0.7rem] flex">
 				<img src={facebook} />
 				<img src={youtube} />
 				<img src={twitter} />
 				<img src={pinterest} />
 				<img src={instagram} />
 			</div>
-			<ol>
+			<ol className="text-white text-center text-[12px] font-[700] tracking-[2px] uppercase list-none">
 				<li>home</li>
 				<li>stories</li>
 				<li>features</li>
 				<li>pricing</li>
 			</ol>
-			<div className="btn-four-box">
+			<div className="btn-four-box mt-[2rem] mb-[3rem]">
 				<button className="btn-four">
 					Get An Invite
 					<img className="arrow-img-light" src={arrow} />
 				</button>
 			</div>
-			<p>Copyright 2023. All Rights Reserved</p>
+			<p className="text-white text-[15px] opacity-[0.5]">
+				Copyright 2023. All Rights Reserved
+			</p>
 		</footer>
 	);
 }
