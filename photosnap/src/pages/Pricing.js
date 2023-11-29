f3dacd59d33ee0eaaa523fb7b672580c9c6084b5
alex sanchez <alexjsanchez1020@gmail.com>
2023-11-29 03:15:21 -0500

    import images

--- photosnap/src/pages/Pricing.js
+++ photosnap/src/pages/Pricing.js
@@ -1,5 +1,38 @@
 import React from "react";
-
+import dusk from "../assets/pricing/mobile/hero.jpg";
+import duskmid from "../assets/pricing/tablet/hero.jpg";
 export default function Pricing() {
-	return <div>Pricing</div>;
+	return (
+		<div>
+			<div id="main-card" className="bg-black tablet:flex">
+				<img
+					id="main-card-top-image "
+					className="w-full tablet:hidden"
+					src={hero}
+					alt="man on mountain"
+				/>
+
+				<div className="main-card-bottom-boommark tablet:hidden"></div>
+				<div
+					id="main-card-bottom"
+					className="tablet:px-[54px] tablet:py-[173px] px-[29px] py-[72px] text-white bg-black "
+				>
+					<h2 className="text-white text-[32px] tablet:text-[40px] font-bold leading-[40px] tablet:leading-[48px] tracking-[3.333px] tablet:tracking-[4.167px] uppercase">
+						FEATURES
+					</h2>
+					<p className="mt-[1rem] mb-[1.438rem] tablet:mt-[21px] text-white text-[15px] leading-[25px] opacity-[0.6]">
+						We make sure all of our features are designed to be loved by every
+						aspiring and even professional photograpers who wanted to share
+						their stories.
+					</p>
+				</div>
+				<img
+					id="main-card-top-image "
+					className="hidden tablet:block"
+					src={heromid}
+					alt="man on mountain"
+				/>
+			</div>
+		</div>
+	);
 }
