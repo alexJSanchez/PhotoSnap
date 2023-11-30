1ae021c2b3b8383b9d2f58ed0d77e7431773b75f
alex sanchez <alexjsanchez1020@gmail.com>
2023-11-30 01:17:08 -0500

    pricing card

--- photosnap/src/pages/Pricing.js
+++ photosnap/src/pages/Pricing.js
@@ -34,7 +34,7 @@ export default function Pricing() {
 					alt="man on mountain"
 				/>
 			</div>
-			<div className="flex justify-center items-center gap-[32px]">
+			<div className="flex justify-center items-center gap-[32px] mt-[64px] mb-[40px]">
 				{discount ? (
 					<p className="text-[18px] font-bold leading-[25px] opacity-50">
 						Monthly
@@ -58,6 +58,22 @@ export default function Pricing() {
 						Yearly
 					</p>
 				)}
+			</div>
+			<div className="flex flex-col mx-7">
+				<div className="text-center bg-[#F5F5F5] pt-[56px] pb-[40px] px-[22px]">
+					<h3 className="text-[24px] font-bold leading-[25px]">Basic</h3>
+					<p className="text-[15px] leading-[25px] opacity-60 mt-[18px] ">
+						Includes basic usage of our platform. Recommended for new and
+						aspiring photographers.
+					</p>
+					<p className="text-[40px] font-bold leading-[48px] tracking-[4.167px] uppercase pt-[40px]">
+						$19.00
+					</p>
+					<p className="text-[15px] leading-[25px opacity-60">per month</p>
+					<button className="bg-black text-white mt-[52px] w-full py-[12px]">
+						pick plan
+					</button>
+				</div>
 			</div>
 		</div>
 	);
